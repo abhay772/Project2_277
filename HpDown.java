84220b150628fed5f3fdd21d640f7a092287d6f5
nathanwolski77 <55123156+nathanwolski77@users.noreply.github.com>
2021-12-03 00:45:27 -0800

    Update HpDown.java

--- HpDown.java
+++ HpDown.java
@@ -1,4 +1,8 @@
 public class HpDown extends PokemonDecorator{
+    /**
+ * adds health debuff to a pokemon
+ * @param p pokemon receiving debuff
+ */
     public HpDown(Pokemon p)
     {
         super(p,"-HP",-1);
