5b2c2c174b681fbc64726e6b18d74dd062b9dcf9
nathanwolski77 <55123156+nathanwolski77@users.noreply.github.com>
2021-12-03 00:43:48 -0800

    Update Grass.java

--- Grass.java
+++ Grass.java
@@ -3,6 +3,11 @@ public class Grass extends Pokemon{
     public Grass(String n, int h, int m) {
         super(n, h, m);
     }
+    /**
+     * Get selected attack menu
+     *
+     * @return String of the selected attack menu
+     */
     public String getAttackMenu(int atkType){
         if(atkType == 1){
             return super.getAttackMenu(atkType);
@@ -10,10 +15,21 @@ public class Grass extends Pokemon{
         else
             return "1. Vine Whip\n2. Razor Leaf\n3. Solar Beam";
     }
+    /**
+     * Get the number of selected attack menu items
+     *
+     * @return int of number of selected attack menu items
+     */
     public int getNumAttackMenuItems(int atkType){
         return 3;
     }
-
+/**
+     * Returns the appropriate attack descriptor for the move
+     *
+     * @param atkType Type of attack: 1) Basic, 2) Special
+     * @param move the selected move
+     * @return String for the selected attack
+     */
     public String getAttackString(int atkType, int move){
 
         System.out.println("Inside Grass");
@@ -30,7 +46,13 @@ public class Grass extends Pokemon{
         };
 
     }
-
+/**
+     * Calculates the damage done by a move
+     *
+     * @param atkType Type of attack: 1) Basic, 2) Special
+     * @param move the selected move
+     * @return damage for the selected attack
+     */
     public int getAttackDamage(int atkType, int move){
         if(atkType == 1)
             return super.getAttackDamage(atkType, move);
@@ -43,10 +65,18 @@ public class Grass extends Pokemon{
                 return (int)(Math.random() * 6) ;
         }
     }
+    /**
+     * Calculates the damage multiplier against Pokemon p
+     *
+     * @param p Pokemon getting receiving damage
+     * @param atkType Type of attack: 1) Basic, 2) Special
+     * @return Damage multiplier for the damage
+     */
     public double getAttackMultiplier(Pokemon p, int atkType){
         if(atkType == 1)
             return 1;
         else
             return  Pokemon.battleTable[1][p.getType()];
     }
+    
 }
