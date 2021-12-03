d245cedca629e9925948ee8517aca18f442107ab
nathanwolski77 <55123156+nathanwolski77@users.noreply.github.com>
2021-12-03 00:45:58 -0800

    Update HpUp.java

--- HpUp.java
+++ HpUp.java
@@ -1,5 +1,8 @@
 public class HpUp extends PokemonDecorator{
-
+/**
+ * adds health buff to a pokemon
+ * @param p pokemon receiving buff
+ */
     public HpUp(Pokemon p)
     {
         super(p,"+HP",(int)(Math.random() * 2) + 1);
