358ced74248c3253b894fc942aeb9a258ddf2e3f
abhay772 <absolanki772@gmail.com>
2021-12-03 10:51:37 +0530

    Update Grass.java

--- Grass.java
+++ Grass.java
@@ -1,22 +1,46 @@
-public interface Grass {
-  public String specialMenu = "1. Vine Whip\n2. Razor Leaf\n3. Solar Beam";
-  public final int numSpecialMenuItems = 3;
-  /**
-    *Performs special move to pokemon
-    *@param p pokemon you wish to attack
-    *@return String of move flavor text
-    */
-  String vineWhip(Pokemon p);
-    /**
-    *Performs special move to pokemon
-    *@param p pokemon you wish to attack
-    *@return String of move flavor text
-    */
-  String razorLeaf(Pokemon p);
-    /**
-    *Performs special move to pokemon
-    *@param p pokemon you wish to attack
-    *@return String of move flavor text
-    */
-  String solarBeam(Pokemon p);
+
+public class Grass extends Pokemon{
+    public Grass(String n, int h, int m) {
+        super(n, h, m);
+    }
+    public String getAttackMenu(int atkType){
+        if(atkType == 1){
+            return super.getAttackMenu(atkType);
+        }
+        else
+            return "1. Vine Whip\n2. Razor Leaf\n3. Solar Beam";
+    }
+    public int getNumAttackMenuItems(int atkType){
+        return 3;
+    }
+    public String getAttackString(int atkType, int move){
+        if(atkType == 1)
+            return super.getAttackString(atkType, move);
+        else{
+            if(move == 1)
+                return "used VINE WHIP on";
+            else if(move == 2)
+                return "used RAZOR LEAF on";
+            else
+                return "used SOLAR BEAM on";
+        }
+    }
+    public int getAttackDamage(int atkType, int move){
+        if(atkType == 1)
+            return super.getAttackDamage(atkType, move);
+        else{
+            if(move == 1)
+                return (int)(Math.random() * 3) + 1;
+            else if(move == 2)
+                return (int)(Math.random() * 3) + 2;
+            else
+                return (int)(Math.random() * 6) ;
+        }
+    }
+    public double getAttackMultiplier(Pokemon p, int atkType){
+        if(atkType == 1)
+            return 1;
+        else
+            return  Pokemon.battleTable[1][p.getType()];
+    }
 }
